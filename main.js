01c46ab3cf5a15f455596723551b8d4e8122cfb8
Aswin-Biju03 <aswinbiju0303@gmail.com>
2026-02-12 09:53:53 +0530

    CRUD APP Completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let users = [];
 
 const form = document.getElementById(`userForm`);
 const tableBody = document.getElementById(`tableBody`);
+const editIndexInput = document.getElementById(`editIndex`);
 
 // add entry to users
 form.addEventListener("submit", (e) => {
@@ -10,7 +11,42 @@ form.addEventListener("submit", (e) => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   //   add name and email to users
-  users.push({ name, email });
+  if (editIndexInput.value == "") {
+    users.push({ name, email });
+  } else {
+    users[editIndexInput.value] = { name, email };
+    editIndexInput.value = "";
+  }
   form.reset();
   console.log(users);
+  displayUsers();
 });
+const displayUsers = () => {
+  tableBody.innerHTML = "";
+  users.forEach((user, index) => {
+    tableBody.innerHTML += `<tr>
+            <td>${index + 1}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>
+              <div class="d-flex gap-2">
+                <button onClick="editUser(${index})" class="btn btn-warning btn-sm">Edit</button>
+                <button onClick="deleteUser(${index})"class="btn btn-danger btn-sm">Delete</button>
+              </div>
+            </td>
+          </tr>`;
+  });
+};
+
+const editUser = (index) => {
+  document.getElementById("name").value = users[index].name;
+  document.getElementById("email").value = users[index].email;
+  editIndexInput.value = index;
+};
+
+const deleteUser = (index) => {
+  if (confirm(`Are you sure you want to delete the data?`)) {
+    users.splice(index, 1);
+    displayUsers();
+  }
+};
